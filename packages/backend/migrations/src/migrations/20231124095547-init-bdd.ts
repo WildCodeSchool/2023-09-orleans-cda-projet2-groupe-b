2bb6480714e45d381aef9cda4bdc5b326b72de30
Joris <joris.pineau8@gmail.com>
2023-11-24 11:37:37 +0100

    init bdd

---
+++ packages/backend/migrations/src/migrations/20231124095547-init-bdd.ts
@@ -0,0 +1,171 @@
+// file format is YYYYMMDDHHMM-<name>.ts
+import type { Kysely } from 'kysely';
+import { sql } from 'kysely';
+
+import type { Database } from '@app/types';
+
+export async function up(db: Kysely<Database>): Promise<void> {
+  // Migration code that update the database to the desired state.
+  await db.schema
+    .createTable('user')
+    .addColumn('id', 'int8', (col) =>
+      col.autoIncrement().primaryKey().unsigned(),
+    )
+    .addColumn('firstname', 'varchar(100)', (col) => col.notNull())
+    .addColumn('lastname', 'varchar(100)', (col) => col.notNull())
+    .addColumn('email', 'varchar(100)', (col) => col.notNull().unique())
+    .addColumn('password', 'varchar(255)', (col) => col.notNull())
+    .addColumn('birthday', 'varchar(10)', (col) => col.notNull())
+    .addColumn('biography', 'text')
+    .addColumn('avatar', 'varchar(255)')
+    .addColumn('passenger_kilometer_traveled', 'decimal(8, 1)', (col) =>
+      col.notNull().unsigned(),
+    )
+    .addColumn('driver_kilometer_traveled', 'decimal(8, 1)', (col) =>
+      col.notNull().unsigned(),
+    )
+    .addColumn('economy_achieved', 'int4', (col) => col.notNull().unsigned())
+    .addColumn('created_at', 'datetime', (col) => col.notNull())
+    .execute();
+
+  await db.schema
+    .createTable('trip')
+    .addColumn('id', 'int8', (col) =>
+      col.autoIncrement().primaryKey().unsigned(),
+    )
+    .addColumn('driver_id', 'int8', (col) =>
+      col.references('driver.id').notNull().unsigned(),
+    )
+    .addColumn('created_at', 'datetime', (col) => col.notNull())
+    .addColumn('date', 'varchar(10)', (col) => col.notNull())
+    .addColumn('kilometer', 'decimal(6, 1)', (col) => col.notNull().unsigned())
+    .addColumn('travel_time', 'varchar(5)', (col) => col.notNull())
+    .addColumn('seat_available', 'int2', (col) => col.notNull().unsigned())
+    .addColumn('price', 'decimal(6, 2)', (col) => col.notNull().unsigned())
+    .addColumn('auto_validation', 'boolean', (col) => col.notNull())
+    .addColumn('comment', 'text')
+    .addColumn('baby_allowed', 'boolean', (col) => col.notNull())
+    .addColumn('non_vaccinated_allowed', 'boolean', (col) => col.notNull())
+    .addColumn('animal_allowed', 'boolean', (col) => col.notNull())
+    .addColumn('smoker_allowed', 'boolean', (col) => col.notNull())
+    .addColumn('with_toll', 'boolean', (col) => col.notNull())
+    .addColumn('car_id', 'int8', (col) =>
+      col.references('car.id').notNull().unsigned(),
+    )
+    .execute();
+
+  await db.schema
+    .createTable('car')
+    .addColumn('id', 'int8', (col) =>
+      col.autoIncrement().primaryKey().unsigned(),
+    )
+    .addColumn('photo', 'varchar(255)')
+    .addColumn('number_seat', 'int2', (col) => col.notNull().unsigned())
+    .addColumn('color', 'varchar(10)', (col) => col.notNull())
+    .addColumn('plate_number', 'varchar(12)', (col) => col.notNull())
+    .addColumn('user_id', 'int8', (col) =>
+      col.references('user.id').notNull().unsigned(),
+    )
+    .addColumn('car_type_id', 'int2', (col) =>
+      col.references('car_type.id').notNull().unsigned(),
+    )
+    .execute();
+
+  await db.schema
+    .createTable('car_type')
+    .addColumn('id', 'int8', (col) =>
+      col.autoIncrement().primaryKey().unsigned(),
+    )
+    .addColumn('brand', 'varchar(15)', (col) => col.notNull())
+    .addColumn('model', 'varchar(25)', (col) => col.notNull())
+    .execute();
+
+  await db.schema
+    .createTable('reservation')
+    .addColumn('id', 'int8', (col) =>
+      col.autoIncrement().primaryKey().unsigned(),
+    )
+    .addColumn('user_id', 'int8', (col) =>
+      col.references('user.id').notNull().unsigned(),
+    )
+    .addColumn('number_seat', 'int2', (col) => col.notNull().unsigned())
+    .addColumn('validated_at', 'datetime', (col) => col.notNull())
+    .addColumn('created_at', 'datetime', (col) => col.notNull())
+    .execute();
+
+  await db.schema
+    .createTable('seat_comparison')
+    .addColumn('id', 'int8', (col) =>
+      col.autoIncrement().primaryKey().unsigned(),
+    )
+    .addColumn('reservation_id', 'int8', (col) =>
+      col.references('reservation.id').notNull().unsigned(),
+    )
+    .addColumn('reserved_seat', 'int2', (col) => col.notNull().unsigned())
+    .addColumn('checkpoint_trip_id', 'int8', (col) =>
+      col.references('checkpoint_trip.id').notNull().unsigned(),
+    )
+    .execute();
+
+  await db.schema
+    .createTable('checkpoint_trip')
+    .addColumn('id', 'int8', (col) =>
+      col.autoIncrement().primaryKey().unsigned(),
+    )
+    .addColumn('start_point', sql`point`, (col) => col.notNull())
+    .addColumn('end_point', sql`point`, (col) => col.notNull())
+    .addColumn('start_address', 'varchar(255)', (col) => col.notNull())
+    .addColumn('end_address', 'varchar(255)', (col) => col.notNull())
+    .addColumn('trip_id', 'int8', (col) =>
+      col.references('trip.id').notNull().unsigned(),
+    )
+    .execute();
+
+  await db.schema
+    .createTable('notice')
+    .addColumn('id', 'int8', (col) =>
+      col.autoIncrement().primaryKey().unsigned(),
+    )
+    .addColumn('note', 'int2', (col) => col.notNull().unsigned())
+    .addColumn('sender_id', 'int8', (col) =>
+      col.references('user.id').notNull().unsigned(),
+    )
+    .addColumn('receiver_id', 'int8', (col) =>
+      col.references('user.id').notNull().unsigned(),
+    )
+    .addColumn('comment', 'text', (col) => col.notNull())
+    .addColumn('sent_at', 'datetime', (col) => col.notNull())
+    .addColumn('trip_id', 'int8', (col) =>
+      col.references('trip.id').notNull().unsigned(),
+    )
+    .execute();
+
+  await db.schema
+    .createTable('messaging')
+    .addColumn('id', 'int8', (col) =>
+      col.autoIncrement().primaryKey().unsigned(),
+    )
+    .addColumn('comment', 'text', (col) => col.notNull())
+    .addColumn('date', 'datetime', (col) => col.notNull())
+    .addColumn('sender_id', 'int8', (col) =>
+      col.references('user.id').notNull().unsigned(),
+    )
+    .addColumn('receiver_id', 'int8', (col) =>
+      col.references('user.id').notNull().unsigned(),
+    )
+    .addColumn('message_read_at', 'datetime', (col) => col.notNull())
+    .execute();
+}
+
+export async function down(db: Kysely<Database>): Promise<void> {
+  // Migration code that reverts the database to the previous state.
+  await db.schema.dropTable('user').ifExists().execute();
+  await db.schema.dropTable('trip').ifExists().execute();
+  await db.schema.dropTable('car').ifExists().execute();
+  await db.schema.dropTable('car_type').ifExists().execute();
+  await db.schema.dropTable('reservation').ifExists().execute();
+  await db.schema.dropTable('seat_comparison').ifExists().execute();
+  await db.schema.dropTable('checkpoint_trip').ifExists().execute();
+  await db.schema.dropTable('notice').ifExists().execute();
+  await db.schema.dropTable('messaging').ifExists().execute();
+}
--- packages/types/src/database.ts
+++ packages/types/src/database.ts
@@ -1,14 +1,137 @@
 import type { Generated, Insertable, Selectable, Updateable } from 'kysely';
 
-export interface ItemTable {
+export interface UserTable {
   id: Generated<number>;
-  content: string;
+  firstname: string;
+  lastname: string;
+  email: string;
+  password: string;
+  birthday: string;
+  biography?: string;
+  avatar?: string;
+  passenger_kilometer_traveled: number;
+  driver_kilometer_traveled: number;
+  economy_achieved: number;
+  created_at: Date;
 }
+export type User = Selectable<UserTable>;
+export type NewUser = Insertable<UserTable>;
+export type UserUpdate = Updateable<UserTable>;
 
-export type Item = Selectable<ItemTable>;
-export type NewItem = Insertable<ItemTable>;
-export type ItemUpdate = Updateable<ItemTable>;
+export interface TripTable {
+  id: Generated<number>;
+  driver_id: number;
+  created_at: Date;
+  date: string;
+  kilometer: number;
+  travel_time: string;
+  seat_available: number;
+  price: number;
+  auto_validation: boolean;
+  comment?: string;
+  baby_allowed: boolean;
+  non_vaccinated_allowed: boolean;
+  animal_allowed: boolean;
+  smoker_allowed: boolean;
+  with_toll: boolean;
+  car_id: number;
+}
+export type Trip = Selectable<TripTable>;
+export type NewTrip = Insertable<TripTable>;
+export type TripUpdate = Updateable<TripTable>;
+
+export interface CarTable {
+  id: Generated<number>;
+  photo: string;
+  number_seat: number;
+  color: string;
+  plate_number: string;
+  user_id: number;
+  car_type_id: number;
+}
+export type Car = Selectable<CarTable>;
+export type NewCar = Insertable<CarTable>;
+export type CarUpdate = Updateable<CarTable>;
+
+export interface CarTypeTable {
+  id: Generated<number>;
+  brand: string;
+  model: string;
+}
+export type CarType = Selectable<CarTypeTable>;
+export type NewCarType = Insertable<CarTypeTable>;
+export type CarTypeUpdate = Updateable<CarTypeTable>;
+
+export interface ReservationTable {
+  id: Generated<number>;
+  user_id: number;
+  number_seat: number;
+  validated_at: Date;
+  created_at: Date;
+}
+export type Reservation = Selectable<ReservationTable>;
+export type NewReservation = Insertable<ReservationTable>;
+export type ReservationUpdate = Updateable<ReservationTable>;
+
+export interface SeatComparisonTable {
+  id: Generated<number>;
+  reservation_id: number;
+  reserved_seat: number;
+  checkpoint_trip_id: number;
+}
+export type SeatComparison = Selectable<SeatComparisonTable>;
+export type NewSeatComparison = Insertable<SeatComparisonTable>;
+export type SeatComparisonUpdate = Updateable<SeatComparisonTable>;
+
+type Point = {
+  x: number;
+  y: number;
+};
+export interface CheckpointTripTable {
+  id: Generated<number>;
+  start_point: Point;
+  end_point: Point;
+  start_address: string;
+  end_address: string;
+  trip_id: number;
+}
+export type CheckpointTrip = Selectable<CheckpointTripTable>;
+export type NewCheckpointTrip = Insertable<CheckpointTripTable>;
+export type CheckpointTripUpdate = Updateable<CheckpointTripTable>;
+
+export interface NoticeTable {
+  id: Generated<number>;
+  note: number;
+  sender_id: number;
+  receiver_id: number;
+  comment: string;
+  sent_at: Date;
+  trip_id: number;
+}
+export type Notice = Selectable<NoticeTable>;
+export type NewNotice = Insertable<NoticeTable>;
+export type NoticeUpdate = Updateable<NoticeTable>;
+
+export interface MessagingTable {
+  id: Generated<number>;
+  comment: string;
+  date: Date;
+  sender_id: number;
+  receiver_id: number;
+  message_read_at: Date;
+}
+export type Messaging = Selectable<MessagingTable>;
+export type NewNMessaging = Insertable<MessagingTable>;
+export type MessagingUpdate = Updateable<MessagingTable>;
 
 export interface Database {
-  item: ItemTable;
+  user: UserTable;
+  trip: TripTable;
+  car: CarTable;
+  car_type: CarTypeTable;
+  reservation: ReservationTable;
+  seat_comparison: SeatComparisonTable;
+  checkpoint_trip: CheckpointTripTable;
+  notice: NoticeTable;
+  messaging: MessagingTable;
 }
