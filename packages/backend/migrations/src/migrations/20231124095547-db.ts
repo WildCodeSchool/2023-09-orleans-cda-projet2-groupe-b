2b75030e005bece149c5ab9d8cd17b5d0bebb0f0
Joris <joris.pineau8@gmail.com>
2023-11-27 17:26:00 +0100

    change requested

--- packages/backend/migrations/src/migrations/20231124095547-db.ts
+++ packages/backend/migrations/src/migrations/20231124095547-db.ts
@@ -6,15 +6,15 @@ import type { Database } from '@app/types';
 
 export async function down(db: Kysely<Database>): Promise<void> {
   // Migration code that reverts the database to the previous state.
-  await db.schema.dropTable('user').ifExists().execute();
+  await db.schema.dropTable('messaging').ifExists().execute();
+  await db.schema.dropTable('notice').ifExists().execute();
+  await db.schema.dropTable('reservation_seat').ifExists().execute();
+  await db.schema.dropTable('reservation').ifExists().execute();
+  await db.schema.dropTable('checkpoint_trip').ifExists().execute();
   await db.schema.dropTable('trip').ifExists().execute();
   await db.schema.dropTable('car').ifExists().execute();
   await db.schema.dropTable('car_type').ifExists().execute();
-  await db.schema.dropTable('reservation').ifExists().execute();
-  await db.schema.dropTable('reservation_seat').ifExists().execute();
-  await db.schema.dropTable('checkpoint_trip').ifExists().execute();
-  await db.schema.dropTable('notice').ifExists().execute();
-  await db.schema.dropTable('messaging').ifExists().execute();
+  await db.schema.dropTable('user').ifExists().execute();
 }
 
 export async function up(db: Kysely<Database>): Promise<void> {
@@ -28,7 +28,7 @@ export async function up(db: Kysely<Database>): Promise<void> {
     .addColumn('lastname', 'varchar(100)', (col) => col.notNull())
     .addColumn('email', 'varchar(100)', (col) => col.notNull().unique())
     .addColumn('password', 'varchar(255)', (col) => col.notNull())
-    .addColumn('birthdate', 'datetime', (col) => col.notNull())
+    .addColumn('birthdate', 'date', (col) => col.notNull())
     .addColumn('biography', 'text')
     .addColumn('avatar', 'varchar(255)')
     .addColumn('passenger_kilometer_traveled', 'decimal(8, 1)', (col) =>
@@ -42,13 +42,37 @@ export async function up(db: Kysely<Database>): Promise<void> {
     .execute();
 
   await db.schema
+    .createTable('car_type')
+    .addColumn('id', 'int8', (col) =>
+      col.autoIncrement().primaryKey().unsigned(),
+    )
+    .addColumn('brand', 'varchar(15)', (col) => col.notNull())
+    .addColumn('model', 'varchar(25)', (col) => col.notNull())
+    .execute();
+
+  await db.schema
+    .createTable('car')
+    .addColumn('id', 'int8', (col) =>
+      col.autoIncrement().primaryKey().unsigned(),
+    )
+    .addColumn('photo', 'varchar(255)')
+    .addColumn('number_seat', 'int2', (col) => col.notNull().unsigned())
+    .addColumn('color', 'varchar(10)', (col) => col.notNull())
+    .addColumn('plate_number', 'varchar(12)', (col) => col.notNull())
+    .addColumn('user_id', 'int8', (col) => col.notNull().unsigned())
+    .addColumn('car_type_id', 'int2', (col) => col.notNull().unsigned())
+    .addForeignKeyConstraint('user_id_fk', ['user_id'], 'user', ['id'])
+    .addForeignKeyConstraint('car_type_id_fk', ['car_type_id'], 'car_type', [
+      'id',
+    ])
+    .execute();
+
+  await db.schema
     .createTable('trip')
     .addColumn('id', 'int8', (col) =>
       col.autoIncrement().primaryKey().unsigned(),
     )
-    .addColumn('driver_id', 'int8', (col) =>
-      col.references('driver.id').notNull().unsigned(),
-    )
+    .addColumn('driver_id', 'int8', (col) => col.notNull().unsigned())
     .addColumn('created_at', 'datetime', (col) => col.notNull())
     .addColumn('date', 'datetime', (col) => col.notNull())
     .addColumn('kilometer', 'decimal(6, 1)', (col) => col.notNull().unsigned())
@@ -62,62 +86,9 @@ export async function up(db: Kysely<Database>): Promise<void> {
     .addColumn('is_animal_allowed', 'boolean', (col) => col.notNull())
     .addColumn('is_smoker_allowed', 'boolean', (col) => col.notNull())
     .addColumn('has_tolls', 'boolean', (col) => col.notNull())
-    .addColumn('car_id', 'int8', (col) =>
-      col.references('car.id').notNull().unsigned(),
-    )
-    .execute();
-
-  await db.schema
-    .createTable('car')
-    .addColumn('id', 'int8', (col) =>
-      col.autoIncrement().primaryKey().unsigned(),
-    )
-    .addColumn('photo', 'varchar(255)')
-    .addColumn('number_seat', 'int2', (col) => col.notNull().unsigned())
-    .addColumn('color', 'varchar(10)', (col) => col.notNull())
-    .addColumn('plate_number', 'varchar(12)', (col) => col.notNull())
-    .addColumn('user_id', 'int8', (col) =>
-      col.references('user.id').notNull().unsigned(),
-    )
-    .addColumn('car_type_id', 'int2', (col) =>
-      col.references('car_type.id').notNull().unsigned(),
-    )
-    .execute();
-
-  await db.schema
-    .createTable('car_type')
-    .addColumn('id', 'int8', (col) =>
-      col.autoIncrement().primaryKey().unsigned(),
-    )
-    .addColumn('brand', 'varchar(15)', (col) => col.notNull())
-    .addColumn('model', 'varchar(25)', (col) => col.notNull())
-    .execute();
-
-  await db.schema
-    .createTable('reservation')
-    .addColumn('id', 'int8', (col) =>
-      col.autoIncrement().primaryKey().unsigned(),
-    )
-    .addColumn('user_id', 'int8', (col) =>
-      col.references('user.id').notNull().unsigned(),
-    )
-    .addColumn('number_seat', 'int2', (col) => col.notNull().unsigned())
-    .addColumn('validated_at', 'datetime', (col) => col.notNull())
-    .addColumn('created_at', 'datetime', (col) => col.notNull())
-    .execute();
-
-  await db.schema
-    .createTable('reservation_seat')
-    .addColumn('id', 'int8', (col) =>
-      col.autoIncrement().primaryKey().unsigned(),
-    )
-    .addColumn('reservation_id', 'int8', (col) =>
-      col.references('reservation.id').notNull().unsigned(),
-    )
-    .addColumn('reserved_seat', 'int2', (col) => col.notNull().unsigned())
-    .addColumn('checkpoint_trip_id', 'int8', (col) =>
-      col.references('checkpoint_trip.id').notNull().unsigned(),
-    )
+    .addColumn('car_id', 'int8', (col) => col.notNull().unsigned())
+    .addForeignKeyConstraint('driver_id_fk', ['driver_id'], 'user', ['id'])
+    .addForeignKeyConstraint('car_id_fk', ['car_id'], 'car', ['id'])
     .execute();
 
   await db.schema
@@ -129,8 +100,41 @@ export async function up(db: Kysely<Database>): Promise<void> {
     .addColumn('end_point', sql`point`, (col) => col.notNull())
     .addColumn('start_address', 'varchar(255)', (col) => col.notNull())
     .addColumn('end_address', 'varchar(255)', (col) => col.notNull())
-    .addColumn('trip_id', 'int8', (col) =>
-      col.references('trip.id').notNull().unsigned(),
+    .addColumn('trip_id', 'int8', (col) => col.notNull().unsigned())
+    .addForeignKeyConstraint('trip_id_fk', ['trip_id'], 'trip', ['id'])
+    .execute();
+
+  await db.schema
+    .createTable('reservation')
+    .addColumn('id', 'int8', (col) =>
+      col.autoIncrement().primaryKey().unsigned(),
+    )
+    .addColumn('user_id', 'int8', (col) => col.notNull().unsigned())
+    .addColumn('number_seat', 'int2', (col) => col.notNull().unsigned())
+    .addColumn('validated_at', 'datetime', (col) => col.notNull())
+    .addColumn('created_at', 'datetime', (col) => col.notNull())
+    .addForeignKeyConstraint('user_id_fk', ['user_id'], 'user', ['id'])
+    .execute();
+
+  await db.schema
+    .createTable('reservation_seat')
+    .addColumn('id', 'int8', (col) =>
+      col.autoIncrement().primaryKey().unsigned(),
+    )
+    .addColumn('reservation_id', 'int8', (col) => col.notNull().unsigned())
+    .addColumn('reserved_seat', 'int2', (col) => col.notNull().unsigned())
+    .addColumn('checkpoint_trip_id', 'int8', (col) => col.notNull().unsigned())
+    .addForeignKeyConstraint(
+      'reservation_id_fk',
+      ['reservation_id'],
+      'reservation',
+      ['id'],
+    )
+    .addForeignKeyConstraint(
+      'checkpoint_trip_id_fk',
+      ['checkpoint_trip_id'],
+      'checkpoint_trip',
+      ['id'],
     )
     .execute();
 
@@ -140,17 +144,14 @@ export async function up(db: Kysely<Database>): Promise<void> {
       col.autoIncrement().primaryKey().unsigned(),
     )
     .addColumn('note', 'int2', (col) => col.notNull().unsigned())
-    .addColumn('sender_id', 'int8', (col) =>
-      col.references('user.id').notNull().unsigned(),
-    )
-    .addColumn('receiver_id', 'int8', (col) =>
-      col.references('user.id').notNull().unsigned(),
-    )
+    .addColumn('sender_id', 'int8', (col) => col.notNull().unsigned())
+    .addColumn('receiver_id', 'int8', (col) => col.notNull().unsigned())
     .addColumn('comment', 'text', (col) => col.notNull())
     .addColumn('sent_at', 'datetime', (col) => col.notNull())
-    .addColumn('trip_id', 'int8', (col) =>
-      col.references('trip.id').notNull().unsigned(),
-    )
+    .addColumn('trip_id', 'int8', (col) => col.notNull().unsigned())
+    .addForeignKeyConstraint('sender_id_fk', ['sender_id'], 'user', ['id'])
+    .addForeignKeyConstraint('receiver_id_fk', ['receiver_id'], 'user', ['id'])
+    .addForeignKeyConstraint('trip_id_fk', ['trip_id'], 'trip', ['id'])
     .execute();
 
   await db.schema
@@ -160,12 +161,10 @@ export async function up(db: Kysely<Database>): Promise<void> {
     )
     .addColumn('comment', 'text', (col) => col.notNull())
     .addColumn('date', 'datetime', (col) => col.notNull())
-    .addColumn('sender_id', 'int8', (col) =>
-      col.references('user.id').notNull().unsigned(),
-    )
-    .addColumn('receiver_id', 'int8', (col) =>
-      col.references('user.id').notNull().unsigned(),
-    )
+    .addColumn('sender_id', 'int8', (col) => col.notNull().unsigned())
+    .addColumn('receiver_id', 'int8', (col) => col.notNull().unsigned())
     .addColumn('message_read_at', 'datetime', (col) => col.notNull())
+    .addForeignKeyConstraint('sender_id_fk', ['sender_id'], 'user', ['id'])
+    .addForeignKeyConstraint('receiver_id_fk', ['receiver_id'], 'user', ['id'])
     .execute();
 }
