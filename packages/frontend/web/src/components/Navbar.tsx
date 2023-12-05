0d1c33d9333d95812114b96dbf4dd5f9e259efe6
Abecker37 <72383676+Abecker37@users.noreply.github.com>
2023-12-05 17:29:05 +0100

    fix: fix link for the image

--- packages/frontend/web/src/components/Navbar.tsx
+++ packages/frontend/web/src/components/Navbar.tsx
@@ -1,15 +1,6 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-import arrowDown from '/public/icons/arrow-down.svg';
-import home from '/public/icons/home.svg';
-import logo from '/public/icons/logo.svg';
-import message from '/public/icons/message.svg';
-import profil from '/public/icons/profil.svg';
-import publish from '/public/icons/publish.svg';
-import rightArrow from '/public/icons/right-arrow.svg';
-import trip from '/public/icons/trip.svg';
-
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -21,9 +12,13 @@ export default function Navbar() {
     <div className='sm:bg-custom-gradient sm:shadow-custom fixed bottom-0 mb-2 ml-[8%] flex w-[84.5%] flex-row justify-start rounded-full border-solid bg-[#008689] text-white duration-75 sm:top-0 sm:mx-0 sm:h-16 sm:w-full sm:justify-between sm:rounded-none'>
       <div className='ml-[8%] mr-[8%] mt-5 duration-75 sm:mt-2 sm:w-[20%] md:mt-2 md:w-[10%]'>
         <Link to={'/'}>
-          <img src={home} alt='home' className='w-7 sm:hidden md:hidden' />
           <img
-            src={logo}
+            src='/icons/home.svg'
+            alt='home'
+            className='w-7 sm:hidden md:hidden'
+          />
+          <img
+            src='/icons/logo.svg'
             alt='logo'
             className='hidden h-full sm:block md:block'
           />
@@ -31,21 +26,21 @@ export default function Navbar() {
       </div>
       <div className='flex flex-row justify-center gap-[3rem] sm:gap-14'>
         <div className='mt-6 hidden h-0 w-0 duration-75 sm:mt-2 sm:flex sm:h-full sm:w-full sm:flex-col'>
-          <img src={publish} alt='publish' className='h-7 w-full' />
+          <img src='/icons/publish.svg' alt='publish' className='h-7 w-full' />
           <p className='text-center'>{'Publish'}</p>
         </div>
         <div className='mt-6 duration-75 sm:mt-2 sm:flex sm:h-full sm:w-full sm:flex-col'>
-          <img src={message} alt='message' className='h-7 w-full' />
+          <img src='/icons/message.svg' alt='message' className='h-7 w-full' />
           <p className='invisible text-center sm:visible md:visible'>
             {'Message'}
           </p>
         </div>
         <div className='mt-6 duration-75 sm:mt-2 sm:flex sm:h-full sm:w-full sm:flex-col'>
-          <img src={trip} alt='trip' className='h-7 w-full' />
+          <img src='/icons/trip.svg' alt='trip' className='h-7 w-full' />
           <p className='invisible text-center sm:visible'>{'Trip'}</p>
         </div>
         <div className='mt-6 duration-75 sm:hidden'>
-          <img src={profil} alt='profil' className='h-7 w-full' />
+          <img src='/icons/profil.svg' alt='profil' className='h-7 w-full' />
         </div>
 
         <div
@@ -53,30 +48,34 @@ export default function Navbar() {
           className='hidden cursor-pointer duration-75 sm:mr-5 sm:mt-2 sm:flex sm:h-full sm:w-full sm:flex-row'
         >
           <div className='flex flex-col'>
-            <img src={profil} alt='profil' className='h-7 w-full' />
+            <img src='/icons/profil.svg' alt='profil' className='h-7 w-full' />
             <p className='invisible text-center sm:visible md:visible'>
               {'Profil'}
             </p>
           </div>
-          <img src={arrowDown} alt='arrow-down' className='ml-1 mr-12 mt-4' />
+          <img
+            src='/icons/arrow-down.svg'
+            alt='arrow-down'
+            className='ml-1 mr-12 mt-4'
+          />
           {isOpen ? (
             <div className='aria-hidden absolute right-2 top-14 mt-2 w-60 rounded-md border bg-[#008689]'>
               <ul className='mb-2 ml-3 mr-6 mt-3'>
                 <li className='flex w-full flex-row justify-between'>
                   <p>{'My information'}</p>
-                  <img src={rightArrow} alt='right-arrow' />
+                  <img src='/icons/right-arrow.svg' alt='right-arrow' />
                 </li>
                 <li className='flex w-full flex-row justify-between'>
                   <p>{'My opinion'}</p>
-                  <img src={rightArrow} alt='right-arrow' />
+                  <img src='/icons/right-arrow.svg' alt='right-arrow' />
                 </li>
                 <li className='flex w-full flex-row justify-between'>
                   <p>{'My preference'}</p>
-                  <img src={rightArrow} alt='right-arrow' />
+                  <img src='/icons/right-arrow.svg' alt='right-arrow' />
                 </li>
                 <li className='flex w-full flex-row justify-between'>
                   <p>{'My car'}</p>
-                  <img src={rightArrow} alt='right-arrow' />
+                  <img src='/icons/right-arrow.svg' alt='right-arrow' />
                 </li>
               </ul>
             </div>
