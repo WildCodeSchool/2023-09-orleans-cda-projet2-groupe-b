0aa946e96a27be49a5fec57c9641ee7273665acc
herve perel <herveperel@gmail.com>
2023-12-06 14:41:47 +0100

    feat frontend

--- packages/frontend/web/src/router.tsx
+++ packages/frontend/web/src/router.tsx
@@ -1,16 +1,11 @@
 import { createBrowserRouter } from 'react-router-dom';
 
 import Home from './pages/Home';
-import Login from './pages/Login';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Home />,
-  },
-  {
-    path: '/login',
-    element: <Login />,
   },
 ]);
 
