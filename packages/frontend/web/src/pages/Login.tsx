0f053fa3642143a6f3f487696657c18115123c94
herve perel <herveperel@gmail.com>
2024-02-28 06:04:41 +0100

    fix login and register

--- packages/frontend/web/src/pages/Login.tsx
+++ packages/frontend/web/src/pages/Login.tsx
@@ -1,5 +1,4 @@
 import { zodResolver } from '@hookform/resolvers/zod';
-import { useState } from 'react';
 import type { SubmitHandler } from 'react-hook-form';
 import { useForm } from 'react-hook-form';
 import { Link, Navigate, useNavigate } from 'react-router-dom';
